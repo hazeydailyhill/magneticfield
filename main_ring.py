bf0ba1b10850d9a575fb7bed70ec15736f55987f
Andrew-Evans-phys <65984308+Andrew-Evans-phys@users.noreply.github.com>
2021-05-17 21:54:17 -0700

    Created new doc for rind sim, added particle dynamics (potential issue)
    Added a seperate file for rings as I felt that it was annoying to constantly go back and forth (maybe I will add an initial config file). I added particle dynamics and noticed that the field does not seem to create closed motion which concerns me. I think that I should observe cyclic motion of the particle, but I do not see this instead things seem to shoot off to infinity, I should look into this. Attemped to add a color heat map to the vector field, but I consistently ran into a problem with the matplotlib GUI, the vectors became large squares. This can be fixed soon, but finals must be finished first. I read about Faraday's and it seems possible. I will need a way of taking surfaces converting to grids and then computing a "mesh" which I can do linear magnetic flux approximations. This, of course will be really computationally expensive and will most likely need to be moved to c++ and use the GPU inorder to have decent load times.

--- main_ring.py
+++ main_ring.py
@@ -2,11 +2,12 @@ import scipy.integrate as integrate
 import scipy.special as special
 import math as m
 import numpy as np
+import matplotlib as mpl 
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 
 def path_func(x,y,z):
-    return np.array([m.cos(15*z),m.sin(15*z),z])
+    return np.array([m.cos(z),m.sin(z),0*z])
 
 def vector_correct(position, input_vector):
     corrected_vector = []
@@ -20,15 +21,12 @@ def Bfield(position, line_direction, line_position):
     current = 1
     return current*np.cross(line_direction/np.linalg.norm(line_direction),((position-line_position)/np.linalg.norm(position-line_position)))/np.linalg.norm(position-line_position)**2
     
-
-#line direction must be a unit vector
 def descrete_integral(steps,start,end,function,position,curve):
     out = 0
     for i in range(1,steps+1):
         out += function(position, curve(0,0,start+(end-start)/steps*i+.01)-curve(0,0,start+(end-start)/steps*i), curve(0,0,start+(end-start)/steps*i)) #wrong
     return out
 
-#ignore this class for now
 class Particle:
     def __init__(self, mass, charge, position, velocity):
         self.mass = mass
@@ -44,44 +42,39 @@ class Particle:
         self.velocity += accel*timestep
         self.position += self.velocity*timestep+.5*accel*timestep**2 
 
-
-
-#p1 = Particle(1, 1, np.array([1.,0.,0.]), np.array([0.,0.,0.]))
-
-
-
 bound_range = 6
 
 output_vectors = []
 for i in range(1,bound_range):
-    for j in range(bound_range-3):
-        for k in range(bound_range+4):
-            start_point = [i-3,j,k-3]
+    for j in range(1,bound_range):
+        for k in range(1,bound_range):
+            start_point = [i-3,j-3,k-3]
             output_vectors.append(vector_correct(start_point, descrete_integral(100, 0., 2*m.pi, Bfield, start_point, path_func)))
 
-
 fig = plt.figure()
-
 # draw vector
 soa = np.array(output_vectors)
-
 X, Y, Z, U, V, W = zip(*soa)
 ax = fig.gca(projection='3d')
 
+#curve
 zline = np.linspace(0, 2*m.pi, 500)
-yline = np.sin(15*zline)
-xline = np.cos(15*zline)
+yline = np.sin(zline)
+xline = np.cos(zline)
+ax.plot3D(xline, yline, 0*zline, 'gray')
 
-
-ax.plot3D(xline, yline, zline, 'gray')
-
-ax.quiver(X, Y, Z, U, V, W, length=.75, normalize=True)
+#vector setup
+M = []
+for i in range(len(U)):
+    M.append(m.sqrt(U[i]**2+V[i]**2+W[i]**2))
+qq = ax.quiver(X, Y, Z, U, V, W, M, cmap= 'autumn', length=.75, normalize=True) #I think the color based magnitude will go here
+plt.colorbar(qq, cmap='autumn')
 ax.set_xlim([-50, 50])
 ax.set_ylim([-50, 50])
 ax.set_zlim([-50, 50])
 ax.set_title("Magnetic field of a solenoid")
 
-p1 = Particle(1,1,np.array([-10.,0.,0.]),np.array([1.,0.,0.]))
+p1 = Particle(20,1,np.array([-2.,0.,0.]),np.array([0.,.1,0.]))
 path_of_particle_x = []
 path_of_particle_y = []
 path_of_particle_z = []
@@ -94,13 +87,9 @@ for counter in range(1000):
 
 ax.scatter3D(path_of_particle_x,path_of_particle_y,path_of_particle_z, color = "red")
 
-
 plt.show()
 
-#fig = plt.figure()
-#ax = plt.axes(projection='3d')
-#ax.scatter3D(xpos, ypos, zpos,c=xpos, cmap='Greens')
-#plt.show()
+
 
 
 
